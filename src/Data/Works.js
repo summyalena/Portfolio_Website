65ecb65fdaf098d5b1a9c0db5c989f400ca6035a
Lena <summyalena@gmail.com>
2023-08-26 15:57:20 +0100

    Reset images

--- src/Data/Works.js
+++ src/Data/Works.js
@@ -1,56 +1,63 @@
-import Image1 from './tastymeals.png'
-
+import Image1 from "./tastymeals.png";
+import Image2 from "./heroorphanage.JPG";
+import Image3 from "./techlandingpage.JPG";
+import Image4 from "./yputh.JPG";
+import Image5 from "../assets/twitter clone.JPG";
 
 const Work = [
-    {
+  {
     id: 1,
-    name: 'Tasty Meals' ,
-    description: 'This is a web Application that does the basic booking functionalities online for home made meals in Nigeria. Built using react and Tailwind CSS.',
-    image: Image1,
-    link: 'https://tastmeals.netlify.app/',
-    techStack: 'Next.js | Tailwind CSS | Firebase ',
-    githubLink: 'https://github.com/summyalena/Tasty-Meals'
-},
- 
-{
+    name: "Twitter Clone",
+    description:
+      "Twitter-clone is a web app that is a replica of the UI of twitter social media web app. I added some basic functionalities like comments, posting and liking a post. Built with Next.js, tailwind CSS, Next Authentication and firebase database. ",
+    image: Image5,
+    link: "https://twitter-clone-alena.vercel.app/",
+    techStack: "Next.js | Tailwind CSS | Firebase ",
+    githubLink: "https://github.com/summyalena/Twitter-clone",
+  },
+  {
     id: 2,
-    name: 'Conference',
-    description: 'This is a website created to share awareness, created with html, css and javascript',
-    image: 'http://www.google.com',
-    link: 'https://www.google.com',
-    techStack: 'Javascript | CSS | HTML ',
-    githubLink: 'https://github.com/summyalena/'
-},
+    name: "Tasty Meals",
+    description:
+      "This is a web Application that does the basic booking functionalities online for home made meals in Nigeria. Built using react and Tailwind CSS.",
+    image: Image1,
+    link: "https://tastmeals.netlify.app/",
+    techStack: "React.js | Vanilla CSS | Firebase ",
+    githubLink: "https://github.com/summyalena/Tasty-Meals",
+  },
 
-{
+  {
     id: 3,
-    name: 'Conference',
-    description: 'This is a website created to share awareness, created with html, css and javascript',
-    image: 'http://www.google.com',
-    link: 'https://www.google.com',
-    techStack: 'Javascript | CSS | HTML ',
-    githubLink: 'https://github.com/summyalena/'
-},
+    name: "Youth Summit",
+    description:
+      "This is a website that advertises a youth conference or summit. This was built with Html, CSS and Javascript.  ",
+    image: Image4,
+    link: "https://summyalena.github.io/YouthSummit/",
+    techStack: "Javascript | CSS | HTML ",
+    githubLink: "https://github.com/summyalena/YouthSummit",
+  },
 
-{
+  {
     id: 4,
-    name: 'Conference',
-    description: 'This is a website created to share awareness, created with html, css and javascript',
-    image: 'http://www.google.com',
-    link: 'https://www.google.com',
-    techStack: 'Javascript | CSS | HTML ',
-    githubLink: 'https://github.com/summyalena/'
-},
+    name: "Orphanage Home",
+    description:
+      "This is a website created to shares awareness of an orphanage home in Enugu city",
+    image: Image2,
+    link: " https://orphanage.vercel.app ",
+    techStack: "Javascript | Next.js | CSS ",
+    githubLink: "https://github.com/summyalena/Orphanage",
+  },
 
-{
+  {
     id: 5,
-    name: 'Conference',
-    description: 'This is a website created to share awareness, created with html, css and javascript',
-    image: 'http://www.google.com',
-    link: 'https://www.google.com',
-    techStack: 'Javascript | CSS | HTML ',
-    githubLink: 'https://github.com/summyalena/'
-}
-]
+    name: "Webxiel Landing page",
+    description:
+      "This is a website created to show the profile of a new tech startup in Nigeria",
+    image: Image3,
+    link: "https://webxiel-testwork67.vercel.app",
+    techStack: "Javascript | React | Vanilla CSS ",
+    githubLink: "https://github.com/summyalena/webxiel_testwork67",
+  },
+];
 
 export default Work;
